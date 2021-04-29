bed9685b267678b485986bf2f09cb7db46470f7b
yazi08 <yaroslav24.01.1998@mail.ru>
2021-04-29 09:41:06 +0300

    Был изменен файл models.py, так же были добавлены новые записи в базу данных

--- blog/home_messages/models.py
+++ blog/home_messages/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    name_blog = models.TextField()
-    data_blog = models.TextField()
-    description_blog = models.TextField(null=True)
-    name_blogers = models.TextField()
+    name_blog = models.TextField('Название')
+    data_blog = models.DateTimeField('Дата публикации')
+    description_blog = models.TextField('Описание', null=True)
+    name_blogers = models.TextField('Имя блогера')
 
     def __str__(self):
         return f"{self.name_blog,self.data_blog,self.description_blog, self.name_blogers}"
